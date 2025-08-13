4389ec6f4e4ccbcda698fe58358adce17e8eab24
Crescent Sambila <crescent.sambila@gmail.com>
2025-08-13 16:27:21 +0300

    Add aut files to /auth route

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from "react-router";
 import Layout from "./layout";
 import Home from "./(index)";
-import Signin from "./signin";
+import Auth from "./auth";
+import SignIn from "./signin";
 import Login from "./login";
 import Board from "./board";
 import Switch from "./(switch)";
@@ -11,7 +12,7 @@ const Index = createBrowserRouter([
   {
     path: "/",
     Component: Layout,
-    children: [Home, Signin, Login, Board, Switch, Shop],
+    children: [Home, Auth, SignIn, Login, Board, Switch, Shop],
   },
 ]);
 
