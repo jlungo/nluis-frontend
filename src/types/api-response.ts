123fb123a7ee64ab8a87252fa6a6646de457bd08
Crescent Sambila <crescent.sambila@gmail.com>
2025-08-22 04:30:41 +0300

    Added results to API response type

--- src/types/api-response.ts
+++ src/types/api-response.ts
@@ -1,7 +1,8 @@
-export interface APIResponse {
+export interface APIResponse<T = unknown> {
   count: number;
   next: string | null;
   previous: string | null;
+  results?: T[];
 }
 
 // "results": [
