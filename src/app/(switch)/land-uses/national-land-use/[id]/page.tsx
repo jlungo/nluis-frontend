01b5252fd08bc715ae3c275b65730caf79e2426d
Crescent Sambila <crescent.sambila@gmail.com>
2025-08-31 02:28:21 +0300

    Created the page to view project

---
+++ src/app/(switch)/land-uses/national-land-use/[id]/page.tsx
@@ -0,0 +1,10 @@
+import ViewProjectPage from '@/components/project/ViewProjectPage';
+
+export default function Page() {
+
+  return (
+    <>
+      <ViewProjectPage moduleLevel="national-land-use" />
+    </>
+  );
+}
