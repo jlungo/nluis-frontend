f654d0ad47e576cd34c2a828575651af9a3d5c00
Crescent Sambila <crescent.sambila@gmail.com>
2025-08-31 02:30:25 +0300

    Added new links

--- src/app/(switch)/land-uses/village-land-use/index.ts
+++ src/app/(switch)/land-uses/village-land-use/index.ts
@@ -1,13 +1,35 @@
-import Layout from "./layout";
+import Layout from "../layout";
 import Page from "./page";
+import type { RouteObject } from "react-router";
 
-const Index = {
+const Index: RouteObject = {
   path: "village-land-use",
   Component: Layout,
   children: [
     {
       index: true,
       Component: Page,
+    },
+    {
+      path: "create",
+      async lazy() {
+        const { default: Component } = await import("./create/page");
+        return { Component };
+      },
+    },
+    {
+      path: ":id",
+      async lazy() {
+        const { default: Component } = await import("./[id]/page");
+        return { Component };
+      },
+    },
+    {
+      path: ":id/edit",
+      async lazy() {
+        const { default: Component } = await import("./[id]/edit/page");
+        return { Component };
+      },
     },
   ],
 };
