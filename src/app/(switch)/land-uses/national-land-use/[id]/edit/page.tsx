29ee088be195ed2451dd710e391dfda1c0107314
Crescent Sambila <crescent.sambila@gmail.com>
2025-08-31 02:28:41 +0300

    Created the page to edit project

---
+++ src/app/(switch)/land-uses/national-land-use/[id]/edit/page.tsx
@@ -0,0 +1,28 @@
+import EditProject from '@/components/project/EditProject';
+import { usePageStore } from '@/store/pageStore';
+import { LOCALITY_LEVELS } from '@/types/constants';
+import { useLayoutEffect } from 'react';
+import { useParams } from 'react-router';
+
+export default function Page() {
+  const { setPage } = usePageStore();
+  const { id } = useParams<{ id: string }>();
+
+  useLayoutEffect(() => {
+    setPage({
+      module: "land-uses",
+      title: "Edit National Land Use Project",
+      backButton: "Back to Modules",
+    });
+  }, [setPage]);
+
+  return (
+    <div>
+      <EditProject 
+        projectId={id as string}
+        moduleLevel={LOCALITY_LEVELS.NATIONAL}
+        afterUpdateRedirectPath="/land-uses/national-land-use"
+      />
+    </div>
+  );
+}
