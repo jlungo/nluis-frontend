de01ada0d1b749ab501489cdddc7a981e6241ff7
Adolf Alfred <adolfalfred321@gmail.com>
2025-10-03 18:46:37 +0300

    Added Table Component

--- src/components/form-field/form-multiselect.tsx
+++ src/components/form-field/form-multiselect.tsx
@@ -1,23 +1,28 @@
 import { Label } from "../ui/label";
 import { Asterisk } from "lucide-react";
 import { MultiSelect } from "../multiselect";
-import type { FieldsProps } from "@/queries/useWorkflowQuery";
+import type { SelectOptionProps } from "@/queries/useWorkflowQuery";
+import { cn } from "@/lib/utils";
 
 interface FormMultiselectProps {
-    data: FieldsProps;
+    name: string;
+    label: string;
+    required: boolean;
+    selectOptions: SelectOptionProps[]
     values?: string[];
     setValues: (values: string[]) => void;
-    disabled?: boolean
+    disabled?: boolean;
+    className?: string;
 }
 
-const FormMultiselect = ({ data, values = [], setValues, disabled }: FormMultiselectProps) => {
-    const { name, label, required, select_options } = data
+const FormMultiselect = (props: FormMultiselectProps) => {
+    const { name, label, required, selectOptions, values = [], setValues, disabled, className } = props
     return (
-        <div className="w-full space-y-2 md:w-[48%] xl:w-[49%]">
+        <div className={cn("w-full space-y-2 md:w-[48%] xl:w-[49%]", className)}>
             <Label htmlFor={name}>{label} {required ? <Asterisk className="text-destructive h-3 w-3" /> : null}</Label>
             <MultiSelect
                 title={label ?? ''}
-                data={select_options.sort((a, b) => a.position - b.position).map(option => ({ label: option.text_label, value: option.text_label }))}
+                data={selectOptions.sort((a, b) => a.position - b.position).map(option => ({ label: option.text_label, value: option.text_label }))}
                 selected={values}
                 setSelected={(e) => setValues(e)}
                 disabled={disabled}
