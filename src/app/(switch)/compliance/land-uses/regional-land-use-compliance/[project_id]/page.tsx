4517d466aa40bb90ee60cb0640dd56790fcc9239
Adolf Alfred <adolfalfred321@gmail.com>
2025-11-23 13:38:27 +0300

    Workflow Modifications

--- src/app/(switch)/compliance/land-uses/regional-land-use-compliance/[project_id]/page.tsx
+++ src/app/(switch)/compliance/land-uses/regional-land-use-compliance/[project_id]/page.tsx
@@ -1,5 +1,5 @@
 import ViewProjectPage from '@/components/compliance-project/ViewProjectPage';
 
 export default function Page() {
-  return <ViewProjectPage moduleLevel="regional-land-use-compliance" />
+  return <ViewProjectPage module='land-uses' moduleLevel="regional-land-use-compliance" />
 }
