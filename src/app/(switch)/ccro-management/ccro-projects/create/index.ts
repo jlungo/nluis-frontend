c5002ec8573059ffd23f104e3116b36051c233c7
rwego@1409 <ludovickpancras@gmail.com>
2025-11-28 00:48:51 +0300

    Merge main: keep subdivision component intact, resolve merge conflicts

--- src/app/(switch)/ccro-management/ccro-projects/create/index.ts
+++ src/app/(switch)/ccro-management/ccro-projects/create/index.ts
@@ -1,10 +1,4 @@
-<<<<<<<< HEAD:src/app/(switch)/ccro-management/ccro-projects/create/index.ts
 import Page from "./page";
-========
-import Layout from "./layout"
-import Page from "./page"
-import Batch from "./[batch]"
->>>>>>>> d33f3ffd17c5e408f56d3aa6ef038f0f6cf86208:src/app/(switch)/land-uses/regional-land-use/[project_id]/[locality_project_id]/questionnaire/[questionnaire_id]/index.ts
 
 const Index = {
   path: "create",
@@ -13,7 +7,6 @@ const Index = {
       index: true,
       Component: Page,
     },
-    Batch,
   ],
 }
 
