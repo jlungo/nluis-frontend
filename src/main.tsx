119f1209cad322d57591c319a5b5a454fededc4c
rwego@1409 <ludovickpancras@gmail.com>
2025-11-28 04:01:15 +0300

    chores

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from "react-router"
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
-import App from './app'
+import App from './app/index'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
